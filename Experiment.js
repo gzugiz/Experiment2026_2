176ae320d44f968714697cbdae3b61fe8c2b75ab
gzugiz <futungineering@gmail.com>
2026-04-14 02:31:00 +0200

    Update Experiment.js

--- Experiment.js
+++ Experiment.js
@@ -2510,6 +2510,7 @@ function TEXTRoutineBegin(snapshot) {
         }
     }
     
+    psychoJS.experiment.addData('actualtext', act);
     mainText.setText(actual);
     TextResp.keys = undefined;
     TextResp.rt = undefined;
